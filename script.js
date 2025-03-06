0b20f3da4be7de5440325a309a8be52d9ff78dc7
Yixuan <90607794+namelesskid123@users.noreply.github.com>
2025-03-06 19:44:36 +0800

    Modifying Number display

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-const Days = document.getElementById("days");
-const Hours = document.getElementById("hours");
 const Minutes = document.getElementById("minutes");
 const Seconds = document.getElementById("seconds");
+const startbutton = document.getElementById("start");
 
-const targetDate = new Date("March 5 2025 13:00:00").getTime();
-
-function timer(){
-    const currentDate = new Date().getTime()
-    const distance = targetDate - currentDate;
-
-    const days = Math.floor(distance / 1000 / 60 / 60 / 24);
-    const hours = Math.floor(distance / 1000 / 60 / 60) % 24;
-    const minutes = Math.floor(distance / 1000 / 60 ) % 60;
-    const seconds = Math.floor(distance / 1000 ) % 60;
-
-    Days.innerHTML = days;
-    Hours.innerHTML = hours;
-    Minutes.innerHTML = minutes;
-    Seconds.innerHTML = seconds;
-
-
-}
+startbutton.addEventListener('click', () => {
+    const mins = Minutes.value;
+    const secs = Seconds.value;
+    if (mins.)
+    console.log(mins, secs);
+})
